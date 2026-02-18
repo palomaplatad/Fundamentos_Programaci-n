8e1e68ca78583ee647d6f4e65fb0b31434618d44
Veronica Alexandra <veronica.micanquer@upb.edu.co>
2026-02-18 09:16:46 -0500

    Condicional Simple

---
+++ 2condicionesSimples/2condicionesSimples/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2condicionesSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string nb = "";
+            float sueldo = 0;
+            Console.WriteLine("Ingrese el nombre del usuario");
+            nb = Console.ReadLine();
+            Console.WriteLine("Ingerese el sueldo");
+            sueldo = Convert.ToSingle(Console.ReadLine());
+            if (sueldo >= 3000)
+            {
+                Console.WriteLine($"El usuerio {nb} debe abonar impuesto");
+
+             
+            }*/
+
+            byte edad = 0;
+            Console.WriteLine("Ingrese su edad");
+            edad= Convert.ToByte(Console.ReadLine());
+            if (edad >= 18)
+            {
+                Console.WriteLine("Bienvenido a mi sitio web");
+            }
+           
+        
+        
+        
+        
+        }
+    }
+}
