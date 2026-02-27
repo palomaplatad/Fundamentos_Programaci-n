f223024055e2964719bdc4e4235922ac81ba8e73
palomaplatad <paloma.platad@gmail.com>
2026-02-27 09:24:48 -0500

    Add files via upload

---
+++ 7.CondicionalesMultiplesOpTer/7.CondicionalesMultiplesOpTer/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.CondicionalesMultiplesOpTer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*char diaSamana = ' ';
+            float totalCompra = 0;
+            float descuento = 0;
+            Console.WriteLine("Ingrese el dia de la semana(l,m,x,j,v,s,d)");
+            diaSamana = Char.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el total compra");
+            totalCompra = Single.Parse(Console.ReadLine());
+
+            switch (diaSamana) 
+            {
+               case 'l':
+                    descuento = totalCompra * 0.1f;
+                    Console.WriteLine($"Hoy es lunes, el descuento es de {descuento} su compra es de {totalCompra - descuento}");
+                    break;
+                case 'm':
+                    descuento = totalCompra * 0.15f;
+                    Console.WriteLine($"Hoy es Martes, el descuento es de {descuento} su compra es de {totalCompra - descuento}");
+                    break;
+                case 'x':
+                    descuento = totalCompra * 0.1f;
+                    Console.WriteLine($"Hoy es Miercoles, el descuento es de {descuento} su compra es de {totalCompra - descuento}");
+                    break;
+                case 'j':
+                    descuento = totalCompra * 0.15f;
+                    Console.WriteLine($"Hoy es Jueves, el descuento es de {descuento} su compra es de {totalCompra - descuento}");
+                    break;
+                    case 'v':
+                    descuento = totalCompra * 0.2f;
+                    Console.WriteLine($"Hoy es Viernes, el descuento es de {descuento} su compra es de {totalCompra - descuento}");
+                    break;
+                case 's':
+                    descuento = totalCompra * 0.2f;
+                    Console.WriteLine($"Hoy es Sabado, el descuento es de {descuento} su compra es de {totalCompra - descuento}");
+                    break;
+                case 'd':
+                    descuento = totalCompra * 0;
+                    Console.WriteLine($"Hoy es Domingo, el descuento es de {descuento} su compra es de {totalCompra - descuento}");
+                    break;
+                default:
+                    Console.WriteLine("Seleccione un dia correcto de la semana");
+                    break;*/
+
+            /*            float n1 = 0;
+                        float n2 = 0;
+                        string operacion = " ";
+                        Console.WriteLine("Ingrses el primer numero");
+                        n1= Single .Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo numero");
+                        n2= Single .Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese la operacion (suma,resta,multiplicación,división)");
+                        operacion = Console.ReadLine();
+
+                        switch (operacion)
+                        {
+                            case "suma":
+                                Console.WriteLine(n1 + n2);
+                                break;
+                            case "resta":
+                                Console.WriteLine(n1 - n2);
+                                break;
+                            case "multiplicación":
+                                Console.WriteLine(n1 * n2);
+                                break;
+                            case "division":
+                                Console.WriteLine(n1 / n2);
+                                break;
+                            default:
+                                Console.WriteLine("Seleccione uno de los operadores disponibles");
+                                break;*/
+            byte Edad = 0;
+            Console.WriteLine("Ingrese la Edad");
+            Edad = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine(Edad >= 18 ? "Bienvenido a mi sitio web" : "No es apto para el cotenido de este sitio web");
+            
+
+
+
+
+
+
+
+
+
+
+
+            
+        
+        
+        
+        
+        
+        
+        
+        }
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+    }
+    
+}
