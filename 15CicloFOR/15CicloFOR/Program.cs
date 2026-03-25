8d2227a6cc2f381371f9427985eda8b8817d5b78
palomaplatad <paloma.platad@gmail.com>
2026-03-25 09:27:13 -0500

    Add files via upload

---
+++ 15CicloFOR/15CicloFOR/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15CicloFOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int acumulador = 0;
+            for (int i = 0; i <= 5; i++)
+            {
+                acumulador += i;
+            }
+            Console.WriteLine (acumulador);*/
+            /*int fact = 1;
+            int n =  0;
+            Console.WriteLine("Ingrese su numero");
+            n= int.Parse(Console.ReadLine());
+            for (int i = 1; i<= n; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine($"El factorial es {fact}");*/
+            int rangoinicio = 0;
+            int rangofinal = 0;
+            Console.WriteLine("ingrese el comienzo del rango");
+            rangoinicio = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ingrese el final del rango");
+            rangofinal = Convert.ToInt32(Console.ReadLine());
+            for (int i = rangoinicio ;i <= rangofinal ;i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine($"{i} es multiplo de 5");
+                }
+
+
+            }
+            
+
+        }
+    }
+}
---
+++ 15CicloFOR/15CicloFOR/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("15CicloFOR")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("15CicloFOR")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("18aa9154-9158-487e-9823-af6520d50799")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
