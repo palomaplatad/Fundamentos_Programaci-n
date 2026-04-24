5e426d908065cdaa430a540f1fb861c81bb259fe
palomaplatad <paloma.platad@gmail.com>
2026-04-24 09:25:59 -0500

    Add files via upload

---
+++ Programacion Modular/Programacion Modular/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programacion_Modular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BorrarPantalla();
+            Console.WriteLine($"Edad: {CalculaEdad()}");
+        }
+        static void BorrarPantalla()
+        {
+            Console.Clear();
+        }
+        static void Saludo()
+        {
+            string nombre = "Paloma";
+            Console.WriteLine($"Bienvenid@ {nombre}");
+        }
+        static int CalculaEdad()
+        {
+            int añoact = 2026;
+            int añonac = 2001;
+            int edad = añoact - añonac;
+            return edad;
+
+        }
+        static int CalcularEdad(int añoactual,int añonacimiento)
+        {
+            return añoactual - añonacimiento;
+        }
+
+    }
+}
